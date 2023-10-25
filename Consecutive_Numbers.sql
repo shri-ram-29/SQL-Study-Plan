0ef0f63c01edb72704d5f8e635078db76e4bcb94
shri-ram-29 <ashriram29@gmail.com>
2023-10-25 10:55:57 +0530

    Consecutive Numbers SQL Code

---
+++ Consecutive_Numbers.sql
@@ -0,0 +1,19 @@
+/*
+Table: Logs
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| num         | varchar |
++-------------+---------+
+In SQL, id is the primary key for this table.
+id is an autoincrement column.
+Find all numbers that appear at least three times consecutively.
+Return the result table in any order.
+The result format is in the following example.
+*/
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 ON l1.id = l2.id - 1
+JOIN Logs l3 ON l1.id = l3.id - 2
+WHERE l1.num = l2.num AND l2.num = l3.num;
