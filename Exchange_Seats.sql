9c8e724e3cfd552028e5198aeab27ffa794d8da1
shri-ram-29 <ashriram29@gmail.com>
2023-10-27 09:59:35 +0530

    Exchange Seats SQL Code

---
+++ Exchange_Seats.sql
@@ -0,0 +1,24 @@
+/*
+Table: Seat
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| student     | varchar |
++-------------+---------+
+id is the primary key (unique value) column for this table.
+Each row of this table indicates the name and the ID of a student.
+id is a continuous increment.
+Write a solution to swap the seat id of every two consecutive students. If the number of students is odd, the id of the last student is not swapped.
+Return the result table ordered by id in ascending order.
+The result format is in the following example.
+*/
+SELECT
+    CASE
+        WHEN id % 2 = 1 AND id = (SELECT MAX(id) FROM Seat) THEN id
+        WHEN id % 2 = 1 THEN id + 1
+        ELSE id - 1
+    END AS id,
+    student
+FROM Seat
+ORDER BY id;
