32d4d7a80c10febe9cdc79a3d265c95188ef5ba9
shri-ram-29 <ashriram29@gmail.com>
2023-10-31 15:45:00 +0530

    Patients With a Condition SQL Code

---
+++ Patients_With_a_Condition.sql
@@ -0,0 +1,19 @@
+/*
+Table: Patients
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| patient_id   | int     |
+| patient_name | varchar |
+| conditions   | varchar |
++--------------+---------+
+patient_id is the primary key (column with unique values) for this table.
+'conditions' contains 0 or more code separated by spaces. 
+This table contains information of the patients in the hospital.
+Write a solution to find the patient_id, patient_name, and conditions of the patients who have Type I Diabetes. Type I Diabetes always starts with DIAB1 prefix.
+Return the result table in any order.
+The result format is in the following example.
+*/
+SELECT patient_id, patient_name, conditions
+FROM Patients
+WHERE conditions LIKE 'DIAB1%' OR conditions LIKE '% DIAB1%'
